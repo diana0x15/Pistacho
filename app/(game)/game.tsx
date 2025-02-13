f4463c9a1e60bc90e66841ffbc5835b2a818481a
diana0x15 <dianapislaru21@gmail.com>
2025-02-13 11:14:38 +0100

    Update the game to highlight the current word.

--- app/(game)/game.tsx
+++ app/(game)/game.tsx
@@ -1,38 +1,57 @@
-import { StyleSheet, View } from 'react-native';
+import { StyleSheet, View, KeyboardAvoidingView, Platform } from "react-native";
 
-import { ThemedText } from '@/components/ThemedText';
-import { ThemedView } from '@/components/ThemedView';
-import { ProgressBar } from '@/components/ProgressBar';
-import BathroomReduced from '@/assets/images/bathroom-reduced.svg';
-import { GameEntry } from '@/components/GameEntry';
-import CrosswordGrid from '@/components/CrosswordGrid';
+import { ThemedText } from "@/components/ThemedText";
+import { ThemedView } from "@/components/ThemedView";
+import { ProgressBar } from "@/components/ProgressBar";
+import BathroomReduced from "@/assets/images/bathroom-reduced.svg";
+import { GameEntry } from "@/components/GameEntry";
+import CrosswordGrid from "@/components/CrosswordGrid";
 
 export default function Category() {
-
   const PADDING_TOP = 80;
 
-  return ( 
-    <ThemedView style={[styles.container, {paddingTop: PADDING_TOP}]}>
-      <View style={styles.progressContainer}>
-        <ProgressBar color={'#75A7D3'} progress={0.2} style='elevated' />
-      </View>
-      <CrosswordGrid />
-    </ThemedView>)
+  return (
+    <ThemedView style={[styles.background, { paddingTop: PADDING_TOP }]}>
+      <KeyboardAvoidingView
+        style={[styles.keyboardAvoidingView]}
+        behavior={Platform.OS === "ios" ? "padding" : "height"}
+      >
+        <View style={styles.progressContainer}>
+          <ProgressBar color={"#75A7D3"} progress={0.2} style="elevated" />
+        </View>
+        <View style={styles.gameContainer}>
+          <CrosswordGrid />
+        </View>
+        <View style={styles.clueContainer}></View>
+      </KeyboardAvoidingView>
+    </ThemedView>
+  );
 }
 
 const styles = StyleSheet.create({
-  container: {
-    padding: 20,
+  background: {
     flex: 1,
   },
+  keyboardAvoidingView: {
+    flex: 1,
+    display: "flex",
+    alignItems: "center",
+    justifyContent: "center",
+  },
   progressContainer: {
-    width: '100%',
-    display: 'flex',
-    alignItems: 'center',
+    width: "100%",
+    display: "flex",
+    alignItems: "center",
     marginBlock: 30,
   },
-  entriesList: {
-    marginTop: 40,
-    gap: 20,
-  }
+  gameContainer: {
+    marginTop: 10,
+    width: "100%",
+  },
+  clueContainer: {
+    width: "100%",
+    height: 80,
+    backgroundColor: "#D7DECC",
+    marginTop: "auto",
+  },
 });
--- components/CrosswordGrid.tsx
+++ components/CrosswordGrid.tsx
@@ -1,15 +1,31 @@
 import React from "react";
-import {useState, useEffect} from "react";
-import { View, Text, StyleSheet, TouchableOpacity, TextInput, Platform, KeyboardAvoidingView, Keyboard } from "react-native";
+import { useState, useRef } from "react";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TouchableOpacity,
+  TextInput,
+} from "react-native";
 
 enum Direction {
   HORIZONTAL = "horizontal",
   VERTICAL = "vertical",
-  BOTH = "both" // Intersection
 }
 
-function computeGridSize(words: any[]) {
-  let maxRow = 0, maxCol = 0;
+type Word = {
+  word: string;
+  row: number;
+  col: number;
+  direction: Direction;
+};
+
+/**
+ * Compute the size of the smallest square grid that fits the game.
+ */
+function computeGridSize(words: Word[]) {
+  let maxRow = 0;
+  let maxCol = 0;
   words.forEach(({ word, row, col, direction }) => {
     if (direction === Direction.HORIZONTAL) {
       maxCol = Math.max(maxCol, col + word.length - 1);
@@ -21,16 +37,22 @@ function computeGridSize(words: any[]) {
   return Math.max(maxRow + 1, maxCol + 1);
 }
 
-function createGrid(words: any[], empty: boolean) {
+/**
+ * Creates a crossword grid for the given words, where negative cells are null.
+ * If empty===true, the grid's cells are will be empty strings.
+ */
+function createGrid(words: Word[], empty: boolean) {
   const gridSize = computeGridSize(words);
-  const grid = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));
+  const grid = Array.from({ length: gridSize }, () =>
+    Array(gridSize).fill(null)
+  );
 
   words.forEach(({ word, row, col, direction }) => {
     for (let i = 0; i < word.length; i++) {
       if (direction === Direction.HORIZONTAL) {
-        grid[row][col + i] = empty ? '' : word[i];
+        grid[row][col + i] = empty ? "" : word[i];
       } else {
-        grid[row + i][col] = empty ? '' : word[i];
+        grid[row + i][col] = empty ? "" : word[i];
       }
     }
   });
@@ -38,32 +60,25 @@ function createGrid(words: any[], empty: boolean) {
 }
 
 /**
- * @param words 
- * @returns A grid that keeps track of the corresponding word(s) for each cell,
- * in the following format: gridMap[i][j] = {first: word1, second: word2}
- * Cells belonging to a single word: gridMap[i][j] = {first: word, second: null}
+ * A grid that keeps track of the corresponding word(s) for each cell,
+ * in the following format: gridMap[i][j] = {horiz: word1, vert: word2}.
+ * Cells belonging to a single word: gridMap[i][j] = {horiz: word, vert: null}
  */
-function createMapGrid(words: any[]) {
+function createMapGrid(words: Word[]) {
   const gridSize = computeGridSize(words);
-  const grid = Array.from({ length: gridSize }, () => Array(gridSize).fill(null));
+  const grid = Array.from({ length: gridSize }, () =>
+    Array(gridSize).fill(null)
+  );
 
   words.forEach((wordEntry) => {
     const { word, row, col, direction } = wordEntry;
     for (let i = 0; i < word.length; i++) {
       if (direction === Direction.HORIZONTAL) {
-        if (grid[row][col + i] === null) {
-          grid[row][col + i] = {first: wordEntry, second: null};
-        } else {
-          const prevCell = grid[row][col + i].first;
-          grid[row][col + i] = {first: prevCell, second: wordEntry};
-        }
+        const vert = grid[row][col + i]?.vert;
+        grid[row][col + i] = { horiz: wordEntry, vert: vert };
       } else {
-        if (grid[row + i][col] === null) {
-          grid[row + i][col] = {first: wordEntry, second: null};
-        } else {
-          const prevCell = grid[row + i][col].first;
-          grid[row + i][col] = {first: prevCell, second: wordEntry};
-        }
+        const horiz = grid[row + i][col]?.horiz;
+        grid[row + i][col] = { horiz: horiz, vert: wordEntry };
       }
     }
   });
@@ -71,7 +86,7 @@ function createMapGrid(words: any[]) {
 }
 
 const CrosswordGrid = () => {
-  const words = [
+  const words: Word[] = [
     { word: "HELLO", row: 3, col: 0, direction: Direction.HORIZONTAL },
     { word: "WORLD", row: 0, col: 2, direction: Direction.VERTICAL },
     { word: "COLD", row: 1, col: 1, direction: Direction.HORIZONTAL },
@@ -80,93 +95,172 @@ const CrosswordGrid = () => {
   const [solutionGrid, setSolutionGrid] = useState(createGrid(words, false));
   const [userGrid, setUserGrid] = useState(createGrid(words, true));
   const [mapGrid, setMapGrid] = useState(createMapGrid(words));
-  const [selectedCell, setSelectedCell] = useState({row: words[0].row, col: words[0].col});
-  const [currentDirection, setCurrentDirection] = useState(Direction.HORIZONTAL);
+  const [selectedCell, setSelectedCell] = useState({
+    row: words[0].row,
+    col: words[0].col,
+  });
+  const [currentDirection, setCurrentDirection] = useState(
+    Direction.HORIZONTAL
+  );
+  const textInput = useRef<TextInput>(null);
 
   function isSelected(row: number, col: number) {
     return selectedCell.row === row && selectedCell.col === col;
   }
 
+  function isHighlighted(row: number, col: number) {
+    const currentWord =
+      currentDirection === Direction.HORIZONTAL
+        ? mapGrid[row][col].horiz
+        : mapGrid[row][col].vert;
+    const highlightedWord =
+      currentDirection === Direction.HORIZONTAL
+        ? mapGrid[selectedCell.row][selectedCell.col].horiz
+        : mapGrid[selectedCell.row][selectedCell.col].vert;
+
+    return currentWord === highlightedWord;
+  }
+
   function onInput(value: string) {
-    if (value.length !== 1 || !value.match(/[A-ZÑÁÉÍÓÚ]/i)) {
+    if (value.length !== 1 || !value.match(/[A-ZÑÁÉÍÓÚÜ]/i)) {
       return;
     }
-    console.log(currentDirection);
-    
+
+    // TODO: backspace deletes the value in the selected cell.
+
     setUserGrid((prevGrid) => {
       const newGrid = prevGrid.map((r, rowIndex) =>
         r.map((cell, colIndex) =>
-          rowIndex === selectedCell.row && colIndex === selectedCell.col ? value : cell
+          rowIndex === selectedCell.row && colIndex === selectedCell.col
+            ? value
+            : cell
         )
       );
       return newGrid;
     });
 
     moveToNextCell();
-
   }
 
+  /**
+   * Selects the cell at the given row and col. Also updates the current
+   * direction to match the direction of the word at the selected cell. If it's
+   * an intersection cell, keep the current direction.
+   */
   function selectCell(row: number, col: number) {
-    if (mapGrid[row][col].second === null) {
-      setCurrentDirection(mapGrid[row][col].first.direction);
-    } else {
+    // If already selected, change direction if possible
+    if (selectedCell.row === row && selectedCell.col === col) {
+      if (mapGrid[row][col].horiz && mapGrid[row][col].vert) {
+        if (currentDirection === Direction.HORIZONTAL) {
+          setCurrentDirection(Direction.VERTICAL);
+        } else {
+          setCurrentDirection(Direction.HORIZONTAL);
+        }
+      }
+      return;
+    }
+
+    // If it's an intersection, keep the same direction. Otherwise, change direction to match the selected word.
+    if (!mapGrid[row][col].horiz) {
+      setCurrentDirection(Direction.VERTICAL);
+    }
+
+    if (!mapGrid[row][col].vert) {
       setCurrentDirection(Direction.HORIZONTAL);
     }
-    setSelectedCell({row, col});
+
+    // Focus on the textInput to show to keyboard.
+    textInput.current?.focus();
+
+    setSelectedCell({ row, col });
   }
 
   function moveToNextCell() {
+    const canGoRight =
+      selectedCell.col < userGrid.length - 1 &&
+      userGrid[selectedCell.row][selectedCell.col + 1] != null;
+    const canGoDown =
+      selectedCell.row < userGrid.length - 1 &&
+      userGrid[selectedCell.row + 1][selectedCell.col] != null;
 
-    const canGoRight = selectedCell.col < userGrid.length - 1 && userGrid[selectedCell.row][selectedCell.col + 1] != null;
-    const canGoDown = selectedCell.row < userGrid.length - 1 && userGrid[selectedCell.row + 1][selectedCell.col] != null;
     if (currentDirection === Direction.HORIZONTAL && canGoRight) {
-      setSelectedCell({row: selectedCell.row, col:selectedCell.col+1});
+      setSelectedCell({ row: selectedCell.row, col: selectedCell.col + 1 });
     }
     if (currentDirection === Direction.VERTICAL && canGoDown) {
-      setSelectedCell({row: selectedCell.row+1, col:selectedCell.col});
+      setSelectedCell({ row: selectedCell.row + 1, col: selectedCell.col });
     }
   }
 
-  const Cell = ({row, col, text }: {row: number, col: number, text: string}) => {
-    return (<TouchableOpacity style={[styles.cell, styles.validCell, isSelected(row, col) ? styles.selectedCell : undefined]} onPress={() => {selectCell(row, col)}}>
-      <Text style={styles.text}>{text}</Text>
-    </TouchableOpacity>)
-  }
+  const Cell = ({
+    row,
+    col,
+    value,
+  }: {
+    row: number;
+    col: number;
+    value: string;
+  }) => {
+    return (
+      <TouchableOpacity
+        style={[
+          styles.cell,
+          styles.validCell,
+          isHighlighted(row, col) ? styles.highlightedCell : undefined,
+          isSelected(row, col) ? styles.selectedCell : undefined,
+        ]}
+        onPress={() => {
+          selectCell(row, col);
+        }}
+      >
+        <Text style={styles.cellContent}>{value}</Text>
+      </TouchableOpacity>
+    );
+  };
 
   const NegativeCell = () => {
-    return (<View style={[styles.cell, styles.negativeCell]} />)
-  }
+    return <View style={[styles.cell, styles.negativeCell]} />;
+  };
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior={Platform.OS === 'ios' ? 'padding' : 'height'}>
-      <TextInput autoFocus={true} style={{opacity: 0}} onKeyPress={(event) => {onInput(event.nativeEvent.key)}} autoCorrect={false} autoCapitalize = {"characters"}/>
+    <View style={styles.container}>
+      <TextInput
+        ref={textInput}
+        autoFocus={true}
+        style={styles.textInput}
+        onKeyPress={(event) => {
+          onInput(event.nativeEvent.key);
+        }}
+        autoCorrect={false}
+        autoCapitalize={"characters"}
+      />
+
       {userGrid.map((row, rowIndex) => (
         <View key={rowIndex} style={styles.row}>
-          {row.map((cell, colIndex) => (
-            (cell !== null) ?
-              <Cell key={colIndex} row={rowIndex} col={colIndex} text={cell} />
-              :
+          {row.map((cell, colIndex) =>
+            cell !== null ? (
+              <Cell key={colIndex} row={rowIndex} col={colIndex} value={cell} />
+            ) : (
               <NegativeCell key={colIndex} />
-          ))}
+            )
+          )}
         </View>
       ))}
-      <View style={styles.clueContainer}></View>
-    </KeyboardAvoidingView>
+    </View>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    padding: 5,
-    display: 'flex',
-    alignItems: 'center',
-    flex: 1,
+    display: "flex",
+    alignItems: "center",
   },
-  clueContainer: {
-    width: '100%',
-    height: 60,
-    backgroundColor: '#D7DECC',
-    marginTop: 'auto',
+  textInput: {
+    position: "absolute",
+    top: -10,
+    left: -10,
+    opacity: 0,
+    width: 1,
+    height: 1,
   },
   row: {
     flexDirection: "row",
@@ -183,13 +277,15 @@ const styles = StyleSheet.create({
     justifyContent: "center",
   },
   negativeCell: {
-    visibility: 'none',
+    visibility: "none",
   },
   selectedCell: {
     backgroundColor: "#F7CD84",
   },
-  text: {
-    fontWeight: "bold",
+  highlightedCell: {
+    backgroundColor: "#CCCBCB",
+  },
+  cellContent: {
     fontSize: 18,
   },
 });
